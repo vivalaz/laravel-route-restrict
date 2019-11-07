88d4aec1625f49cbdaa7ad0936898aa3b3eb9c04
tlgrinevich <grinevich@transloyd.com>
2019-11-07 22:07:50 +0300

    Added  migrations; config; models folders

---
+++ config/laravel-route-restrict.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'models' => [
+        'route' => Vivalaz\LaravelRouteRestrict\Models\Route::class
+    ],
+
+    'table_names' => [
+        'routes' => 'routes',
+        'route_has_roles' => 'route_has_roles',
+        'route_has_permissions' => 'route_has_permissions'
+    ]
+    
+];
---
+++ src/Models/Route.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Vivalaz\LaravelRouteRestrict\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Route extends Model
+{
+
+}
