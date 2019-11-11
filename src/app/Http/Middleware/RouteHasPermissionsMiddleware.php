26bdfeff8c8332baf8716bf773ac4f9f79182cc0
tlgrinevich <grinevich@transloyd.com>
2019-11-11 19:41:49 +0300

    Refactored middleware

--- src/app/Http/Middleware/RouteHasPermissionsMiddleware.php
+++ src/app/Http/Middleware/RouteHasPermissionsMiddleware.php
@@ -5,16 +5,16 @@ namespace Vivalaz\LaravelRouteRestrict\app\Http\Middleware;
 use Closure;
 use Vivalaz\LaravelRouteRestrict\app\Exceptions\RouteUnauthorizedException;
 
-class RouteRoleMiddleware
+class RouteHasPermissionsMiddleware
 {
 
     public function handle($request, Closure $next)
     {
-        if (auth()->user()->hasRouteAccessViaRoles($request->path())) {
+        if (auth()->user()->hasRouteAccessViaPermissions($request->path())) {
             return $next($request);
         }
 
-        throw RouteUnauthorizedException::roles();
+        throw RouteUnauthorizedException::permissions();
     }
 
 }
